c74f3cf38e9694c622a11b156a1e7bccfd209fcb
Bogdan Pantelimon <bogdanpantelimon98@gmail.com>
2025-08-15 00:32:24 +0300

    added mysql connection and automigrate

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/bogdanpantelimon/financetracker/internal/datasource"
 	"github.com/bogdanpantelimon/financetracker/internal/model"
 )
 
 func main() {
+
+	dsn := "user:password@tcp(127.0.0.1:3307)/db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := datasource.ConenctToMySQL(dsn)
+	if err != nil {
+		panic("failed to connect to database: " + err.Error())
+	}
+
+	db.AutoMigrate(&model.Expense{})
+	db.AutoMigrate(&model.Income{})
 
 	expense := model.Expense{
 		Id:   1,
